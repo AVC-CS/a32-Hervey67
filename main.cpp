4bf62547624148b5041bddea1c3d270617a43cfb
Hervey67 <herv2007@gmail.com>
2026-03-12 00:21:34 -0700

    Changes to main.cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,17 @@ int main()
 {
   int numFemale, numMale, numOthers;
   double percF, percM, percO;
+  double total;
 
   cout << "Enter the number of students: Male, Female and Others";
   cin >> numMale >> numFemale >> numOthers;
   // TODO
+
+  total = numMale + numFemale + numOthers;
+  percM = (numMale / total) * 100;
+  percF = (numFemale / total) * 100;
+  percO = (numOthers / total) * 100;
+
 
   cout << setprecision(2) << fixed;
   cout << "Percentage of Male: " << percM << endl;
